2139c182f9a8784f2a874f7f184953030e8ae04a
Jason Chen <jasonschen3@gmail.com.>
2024-06-19 15:05:27 -0600

    Add project cards

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("slide-in-left");
+        observer.unobserve(entry.target); // Stop observing after animation
+      }
+    });
+  });
+
+  var target = document.getElementById("stockbot");
+  observer.observe(target);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("slide-in-left");
+        observer.unobserve(entry.target); // Stop observing after animation
+      }
+    });
+  });
+
+  var target = document.getElementById("prisoner-escape");
+  observer.observe(target);
+});
