1dd2251906e7da5a51d30949b996b39585224f99
Jason Chen <jasonschen3@gmail.com.>
2024-06-19 22:26:02 -0600

    Finish project about contact sections

--- script.js
+++ script.js
@@ -25,3 +25,59 @@ document.addEventListener("DOMContentLoaded", function () {
   var target = document.getElementById("prisoner-escape");
   observer.observe(target);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("slide-in-left");
+        observer.unobserve(entry.target); // Stop observing after animation
+      }
+    });
+  });
+
+  var target = document.getElementById("political-bias");
+  observer.observe(target);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("slide-in-left");
+        observer.unobserve(entry.target); // Stop observing after animation
+      }
+    });
+  });
+
+  var target = document.getElementById("get-gt");
+  observer.observe(target);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("slide-in-left");
+        observer.unobserve(entry.target); // Stop observing after animation
+      }
+    });
+  });
+
+  var target = document.getElementById("about-card");
+  observer.observe(target);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("slide-in-left");
+        observer.unobserve(entry.target); // Stop observing after animation
+      }
+    });
+  });
+
+  var target = document.getElementById("contact-card");
+  observer.observe(target);
+});
